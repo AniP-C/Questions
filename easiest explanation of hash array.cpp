6454cf412dfae6cf5c7ab513d1b382a4dfd353a6
AniP-C <68547683+AniP-C@users.noreply.github.com>
2023-08-04 01:19:09 +0530

    Update easiest explanation of hash array.cpp

--- easiest explanation of hash array.cpp
+++ easiest explanation of hash array.cpp
@@ -5,15 +5,15 @@ using namespace std;
 int main() {
     int n = 5;
     int arr[5] = {1,4,1,6,7};
-    int zrr[8] = {0};
+    int zrr[8] = {0}; // creating empty array
     
     for (int i= 0 ; i < n ; i++) {
-        zrr[arr[i]] = zrr[arr[i]] +1; // adding elements 
+        zrr[arr[i]] = zrr[arr[i]] +1; // adding elements to array after finding them
     }
     
     
     for (int k = 1; k < n ; k++){
-        cout << zrr[k] << endl ;
+        cout << zrr[k] << endl ; // fetching or printing the occursences 
     }
 
 
